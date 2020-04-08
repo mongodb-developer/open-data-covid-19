21e5636f915974c8d93b0da907c093bb96fe9f0a
Maxime Beugnet <maxime.beugnet@gmail.com>
2020-04-08 11:03:28 +0200

    fix url github

--- data-import/0-download-latest-JHU.sh
+++ data-import/0-download-latest-JHU.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rm -rf jhu
-git clone --depth=1 git@github.com:CSSEGISandData/COVID-19.git jhu
+git clone --depth=1 https://github.com/CSSEGISandData/COVID-19.git jhu
 rm -rf jhu/.git
