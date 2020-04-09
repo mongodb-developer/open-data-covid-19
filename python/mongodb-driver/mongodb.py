611a744b7596aad6447d26703573566db2159cf1
Maxime Beugnet <maxime.beugnet@gmail.com>
2020-04-09 16:11:26 +0200

    adding code sample for python & pymongo

--- python/mongodb-driver/mongodb.py
+++ python/mongodb-driver/mongodb.py
@@ -1,13 +1,28 @@
-from datetime import datetime, timedelta
-
+import pymongo
 from pymongo import MongoClient
 
 
 def main():
     client = MongoClient('mongodb+srv://readonly:readonly@covid-19.hip2i.mongodb.net/test?retryWrites=true&w=majority')
-    coll = client.get_database('coronavirus').get_collection('statistics')
-    two_days_ago = datetime.now() - timedelta(days=2)
-    docs = list(coll.find({'country': 'France', 'date': {'$gt': two_days_ago}}))
+    stats = client.get_database('coronavirus').get_collection('statistics')
+    metadata = client.get_database('coronavirus').get_collection('metadata')
+
+    print('Statistics for France, sorted by date descending, limit 20.')
+    print_list(stats.find({'country': 'France', 'state': None}).sort('date', pymongo.DESCENDING).limit(15))
+
+    print('\nMetadata')
+    print_doc(metadata.find_one())
+
+    print('\nIndex of the statistics collection.')
+    print_list(stats.list_indexes())
+
+
+def print_doc(doc):
+    for k, v in doc.items():
+        print(k, '=', v)
+
+
+def print_list(docs):
     for d in docs:
         print(d)
 
