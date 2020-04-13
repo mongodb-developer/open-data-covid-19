e8f047a83fd07d2ed680db2d5d1fb1d764560b9d
Mark Smith <mark.smith@practicalpoetry.co.uk>
2020-04-13 13:37:27 +0100

    Change queries to UK and format the results as a table.

--- python/mongodb-driver/mongodb.py
+++ python/mongodb-driver/mongodb.py
@@ -1,5 +1,8 @@
+#!python3
+
 import pymongo
 from pymongo import MongoClient
+from tabulate import tabulate
 
 
 def main():
@@ -7,24 +10,36 @@ def main():
     stats = client.get_database('covid19').get_collection('statistics')
     metadata = client.get_database('covid19').get_collection('metadata')
 
-    print('Statistics for France, sorted by date descending, limit 20.')
-    print_list(stats.find({'country': 'France', 'state': None}).sort('date', pymongo.DESCENDING).limit(15))
-
-    print('\nMetadata')
-    print_doc(metadata.find_one())
-
-    print('\nIndex of the statistics collection.')
-    print_list(stats.list_indexes())
+    print('Most recent 10 statistics for the UK:')
+    results = stats.find({'country': 'United Kingdom', "state": None}).sort('date', pymongo.DESCENDING).limit(10)
+    print_table(
+        ["date", "confirmed", "deaths"],
+        results)
 
 
-def print_doc(doc):
-    for k, v in doc.items():
-        print(f"{k}={v}")
+def print_table(doc_keys, search_results, headers=None):
+    """
+    Utility function to print a query result as a table.
+
+    Params:
+        doc_keys: A list of keys for data to be extracted from each document.
+        search_results: A MongoDB cursor.
+        headers: A list of headers for the table. If not provided, attempts to
+            generate something sensible from the provided `doc_keys`
+    """
+    if headers is None:
+        headers = [key.replace("_", " ").replace("-", " ").title() for key in doc_keys]
+    records = (extract_tuple(doc, doc_keys) for doc in search_results)
+    print(tabulate(records, headers=headers))
 
 
-def print_list(docs):
-    for d in docs:
-        print(d)
+def extract_tuple(mapping, keys):
+    """
+    Extract a tuple from a mapping by requesting a sequence of keys.
+    
+    Missing keys will result in `None` values in the resulting tuple.
+    """
+    return tuple([mapping.get(key) for key in keys])
 
 
 if __name__ == '__main__':
