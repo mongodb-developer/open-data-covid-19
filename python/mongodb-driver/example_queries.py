45b39115bb3e5d0f529b2dbb52280323f0ff1af0
Mark Smith <mark.smith@practicalpoetry.co.uk>
2020-04-13 13:39:22 +0100

    Blacken the code.

--- python/mongodb-driver/example_queries.py
+++ python/mongodb-driver/example_queries.py
@@ -6,15 +6,19 @@ from tabulate import tabulate
 
 
 def main():
-    client = MongoClient('mongodb+srv://readonly:readonly@covid-19.hip2i.mongodb.net/test?retryWrites=true&w=majority')
-    stats = client.get_database('covid19').get_collection('statistics')
-    metadata = client.get_database('covid19').get_collection('metadata')
+    client = MongoClient(
+        "mongodb+srv://readonly:readonly@covid-19.hip2i.mongodb.net/test?retryWrites=true&w=majority"
+    )
+    stats = client.get_database("covid19").get_collection("statistics")
+    metadata = client.get_database("covid19").get_collection("metadata")
 
-    print('Most recent 10 statistics for the UK:')
-    results = stats.find({'country': 'United Kingdom', "state": None}).sort('date', pymongo.DESCENDING).limit(10)
-    print_table(
-        ["date", "confirmed", "deaths"],
-        results)
+    print("Most recent 10 statistics for the UK:")
+    results = (
+        stats.find({"country": "United Kingdom", "state": None})
+        .sort("date", pymongo.DESCENDING)
+        .limit(10)
+    )
+    print_table(["date", "confirmed", "deaths"], results)
 
 
 def print_table(doc_keys, search_results, headers=None):
@@ -42,5 +46,5 @@ def extract_tuple(mapping, keys):
     return tuple([mapping.get(key) for key in keys])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
