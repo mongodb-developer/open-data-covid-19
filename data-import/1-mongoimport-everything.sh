71c70008b7cfe5797c7fa263f89737d605197b83
Maxime Beugnet <maxime.beugnet@gmail.com>
2020-04-24 02:47:19 +0200

    add exit code check

--- data-import/1-mongoimport-everything.sh
+++ data-import/1-mongoimport-everything.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
+exit_code=0
 mongo "${1}" --quiet --eval "db.dropDatabase()"
+exit_code=$((exit_code + $?))
 
-for file in jhu/csse_covid_19_data/csse_covid_19_daily_reports/*.csv
-do
+for file in jhu/csse_covid_19_data/csse_covid_19_daily_reports/*.csv; do
   mongoimport --uri "${1}" --collection daily --type csv --headerline --file "${file}" &
+  exit_code=$((exit_code + $?))
 done
 wait
 
-for file in jhu/csse_covid_19_data/csse_covid_19_daily_reports_us/*.csv
-do
+for file in jhu/csse_covid_19_data/csse_covid_19_daily_reports_us/*.csv; do
   mongoimport --uri "${1}" --collection daily_us --type csv --headerline --file "${file}" &
+  exit_code=$((exit_code + $?))
 done
 wait
 
-for file in jhu/csse_covid_19_data/csse_covid_19_time_series/*.csv
-do
+for file in jhu/csse_covid_19_data/csse_covid_19_time_series/*.csv; do
   mongoimport --uri "${1}" --collection "$(basename "${file}" .csv)" --type csv --headerline --file "${file}" &
+  exit_code=$((exit_code + $?))
 done
 wait
 
 mongoimport --uri "${1}" --collection UID_ISO_FIPS_LookUp_Table --type csv --headerline --file jhu/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv
+exit_code=$((exit_code + $?))
 
-echo "Job done!"
+echo "$0 finished with code $exit_code."
+exit $exit_code
